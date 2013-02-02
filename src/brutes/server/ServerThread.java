380c178208f843d47d1acfeb96271c5a7deb0244
Olivarès Georges <olivares.georges@gmail.com>
2013-02-02 21:13:12 +0100

    options

--- src/brutes/server/ServerThread.java
+++ src/brutes/server/ServerThread.java
@@ -7,8 +7,14 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
+import java.sql.PreparedStatement;
+import java.util.Iterator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
 
 /**
  *
@@ -18,34 +24,55 @@ public class ServerThread extends Thread {
 
     public static final int TIMEOUT_ACCEPT = 10000;
     public static final int TIMEOUT_CLIENT = 2000;
-    public static final boolean SERVER_MySQL = true;
+    private static String SERVER_TYPE = "";
+    private static String SERVER_HOST = "";
+    private static String SERVER_LOGIN = "";
+    private static String SERVER_PASSWORD = "";
+    private static boolean SERVER_POPULATE = false;
 
     @Override
     public void run() {
+        // Options
+        try {
+            if( !(new File("res/options.xml")).exists()) {
+                System.out.println("Create the file res/options.xml. See res/options.default.xml for more explanation.");
+            }
+            
+            SAXBuilder sxb = new SAXBuilder();
+            Element xmlServer = sxb.build("res/options.xml").getRootElement();
+            for (Iterator<Element> i = xmlServer.getChildren("server").iterator(); i.hasNext();) {
+                Element current1 = i.next();
+                if (current1.getChild("type") != null) {
+                    ServerThread.SERVER_TYPE = current1.getChild("type").getText();
+                }
+                if (current1.getChild("host") != null) {
+                    ServerThread.SERVER_HOST = current1.getChild("host").getText();
+                }
+                if (current1.getChild("login") != null) {
+                    ServerThread.SERVER_LOGIN = current1.getChild("login").getText();
+                }
+                if (current1.getChild("password") != null) {
+                    ServerThread.SERVER_PASSWORD = current1.getChild("password").getText();
+                }
+                if (current1.getChild("populate") != null) {
+                    ServerThread.SERVER_POPULATE = current1.getChild("populate").getText().equals("true");
+                }
+            }
+        } catch (JDOMException | IOException ex) {
+            Logger.getLogger(ServerThread.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        // Server
         try (ServerSocket sockserv = new ServerSocket(42666)) {
             sockserv.setSoTimeout(ServerThread.TIMEOUT_ACCEPT);
+            
+            // Create database connection
+            DatasManager.getInstance(ServerThread.SERVER_TYPE, ServerThread.SERVER_HOST, ServerThread.SERVER_LOGIN, ServerThread.SERVER_PASSWORD);
 
-            boolean toPopulate = false;
-            if (!ServerThread.SERVER_MySQL) {
-                // DEBUG
-                (new File("~$bdd.db")).delete();
-                toPopulate = true;
-
-                //File file = new File("~$bdd.db");
-                //boolean toPopulate = !file.exists();
-                DatasManager.getInstance("sqlite", "~$bdd.db");
-                System.out.println("SQLite");
-            } else {
-                DatasManager.getInstance("mysql", "199.16.131.115/olivares_tp_brute", "olivares_brute", "TPc9hBEzVM2S");
-                //DatasManager.getInstance("mysql", "sql2.freesqldatabase.com:3306/sql22967", "sql22967", "hI3%yA5*");
-                System.out.println("MySQL");
-            }
-
-            if (toPopulate) {
+            if (ServerThread.SERVER_POPULATE) {
                 DatasManager.populate();
-                System.out.println("populate");
             }
-
+            
             while (!this.isInterrupted()) {
                 try {
                     final Socket sockcli = sockserv.accept();
@@ -66,5 +93,6 @@ public class ServerThread extends Thread {
         } catch (IOException ex) {
             Logger.getLogger(ServerThread.class.getName()).log(Level.SEVERE, null, ex);
         }
+        // */
     }
 }
